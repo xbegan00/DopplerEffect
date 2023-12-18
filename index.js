49cd9baa3f8a4e55630bfbedca15a854c8b7c5cb
Mojmír Begán <124684833+xbegan00@users.noreply.github.com>
2023-12-18 11:42:03 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -4,18 +4,13 @@ const sampleRate = 44100;
 // create a typed array of size 44100 float numbers
 const sineWaveArray = new Float32Array(sampleRate);
 
-const hz = 440;
+const hz = 400;
 
 // fill all 44100 elements of array with Math.sin() values
 for (i = 0; i < sineWaveArray.length; i++) {
   sineWaveArray[i] = Math.sin(i * Math.PI * 8 / hz);
 }
 
-/**
- * Play sound in browser
- * @param array - array of values from -1 to +1 representing sound
- * @param sampleRate - sampling rate to play with, e.g. 44100
- */
 function playSound({ array, sampleRate }) {
   // We have to start with creating AudioContext
   const audioContext = new AudioContext({sampleRate});
@@ -27,12 +22,9 @@ function playSound({ array, sampleRate }) {
   const source = audioContext.createBufferSource();
   source.connect(audioContext.destination);
   source.buffer = audioBuffer;
-  source.start();
-  console.log('ugh?');
-  console.log('playing');
-  console.log(array);
+  source.start();  
 }
 
-function playSineWave440hz() {
+function playSineWave400hz() {
   playSound({ array: sineWaveArray, sampleRate });
 }
